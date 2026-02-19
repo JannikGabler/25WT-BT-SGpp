01eb5b362843f660ec373aebe2dfb8095ee30376
JannikGabler <81686534+JannikGabler@users.noreply.github.com>
2026-02-19 01:44:01 +0100

    Added utilities for multiindices

---
+++ combigrid/src/sgpp/combigrid/tools/multiindex/multiindex_utilities.hpp
@@ -0,0 +1,16 @@
+#ifndef COMBIGRID_TOOLS_MULTIINDEX_UTILITIES_HPP
+#define COMBIGRID_TOOLS_MULTIINDEX_UTILITIES_HPP
+
+#include <sgpp/combigrid/type_defs.hpp>
+
+namespace sgpp {
+namespace combigrid {
+namespace tools {
+
+size_t numberOfMIWithComponentSum(size_t nDim, MIType sumOfComponents);
+
+}
+}  // namespace combigrid
+}  // namespace sgpp
+
+#endif
